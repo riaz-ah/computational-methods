5e7e9d7543928d2087025ff33e9e3c8ccbc1c72c
riaz-ah <riazahmadjmu@gmail.com>
2024-03-28 13:24:20 +0100

    Commit test

--- quantum_computing.py
+++ quantum_computing.py
@@ -7,8 +7,8 @@ from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 from numpy import pi
 
 qreg_q = QuantumRegister(2, 'q')
-
 circuit = QuantumCircuit(qreg_q)
 
 circuit.h(qreg_q[0])
 circuit.cx(qreg_q[0], qreg_q[1])
+# this is a test commit message
